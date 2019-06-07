ab14da34c5e5077c97bd88a231962a05974f6f88
brent-soles <b.d.soles@gmail.com>
2019-06-07 00:12:35 -0700

    styling: modified 'inactive' btn style
    Made inactive buttons to have less of a shadow, in order to
    mimic what it would be like IRL.

--- src/components/canvas-button.tsx
+++ src/components/canvas-button.tsx
@@ -40,7 +40,7 @@ export default class SelectButton extends Component<ISelectButtonProps> {
       style={{
         background: `radial-gradient(closest-side, rgba(${r + 100}, ${g + 100}, ${b + 100}, ${a}), rgba(${r}, ${g}, ${b}, ${a}))`,
         borderBottom: `3px solid rgba(${r - 20}, ${g - 20}, ${b - 20}, ${a})`,
-        boxShadow: `0px 2px 20px 1px rgba(${r}, ${g}, ${b}, .7)`
+        boxShadow: state.active ? `0px 2px 20px 1px rgba(${r}, ${g}, ${b}, .7)` : `0px 2px 11px 0px rgba(${r}, ${g}, ${b}, .5)`
       }}
 
       onClick={() => {
@@ -48,7 +48,7 @@ export default class SelectButton extends Component<ISelectButtonProps> {
           waveType: props.waveType,
           rgba: props.bgRgba
         })
-        this.setState({ active: !state.active });
+        this.setState({ active: true });
         this.props.pubFn('canvasButtons', { action: `SET_INACTIVE-${this.props.waveType}`});
       }}
     ></button>
